fc8654a7a9a2057c38fe6951900380fe68967ae1
luanaassis <luanaassis0908@gmail.com>
2023-07-29 14:30:03 -0300

    Refactoring of message tests

--- tests/message_tests.py
+++ tests/message_tests.py
@@ -1,32 +1,27 @@
 import unittest
 from classes.message import Message
 
-
 class MessageTests(unittest.TestCase):
 
-    def test_username(self):
+    def test_user_name(self):
         message = Message("user_name", "text_message", "chat_message")
-
         self.assertEqual(message.user_name, "user_name")
         self.assertNotEqual(message.user_name, "name")
-
+    
     def test_text(self):
         message = Message("user_name", "text_message", "chat_message")
-
         self.assertEqual(message.text, "text_message")
         self.assertNotEqual(message.text, "text")
+    
+    def test_login_message(self):
+        message = Message("user_name", "text_message", "login_message")
+        self.assertEqual(message.message_type, "login_message")
+        self.assertNotEqual(message.message_type, "chat_message")
 
-    def test_message_type(self):
-        message1 = Message("user_name", "text_message", "chat_message")
-
-        self.assertEqual(message1.message_type, "chat_message")
-        self.assertNotEqual(message1.message_type, "login_message")
-
-        message2 = Message("user_name", "text_message", "login_message")
-
-        self.assertEqual(message2.message_type, "login_message")
-        self.assertNotEqual(message2.message_type, "chat_message")
-
+    def test_chat_message(self):
+        message = Message("user_name", "text_message", "chat_message")
+        self.assertEqual(message.message_type, "chat_message")
+        self.assertNotEqual(message.message_type, "login_message")
 
 if __name__ == '__main__':
     unittest.main()
