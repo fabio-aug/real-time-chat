7f0818da0f5e3ac996e52163dcc4bc236cbf086d
SiegLander <71511029+SiegLander@users.noreply.github.com>
2023-07-30 17:51:47 -0300

    Latency test now working
    Doesn't  work all the time, probably because of selenium working faster than app

--- tests/chat_test.py
+++ tests/chat_test.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 
@@ -25,11 +26,11 @@ class ChatTests(unittest.TestCase):
             driver_receive.get('http://localhost:3000/')
 
             # Aguarde um tempo suficiente para que a mensagem seja recebida e a imagem seja renderizada
-            canvas_receive = WebDriverWait(driver_receive, timeout=10).until(lambda d: d.find_element(By.TAG_NAME, 'flt-glass-pane'))
+            canvas_receive = WebDriverWait(driver_receive, timeout=20).until(lambda d: d.find_element(By.TAG_NAME, 'flt-glass-pane'))
             print(canvas_receive)
             # Obtenha o elemento "canvas" no navegador do usuário que recebe para obter o conteúdo inicial
             shadow_root = canvas_receive.shadow_root
-            canvas_shadow = shadow_root.find_element(By.XPATH, '//canvas[1]')
+            canvas_shadow = shadow_root.find_element(By.CSS_SELECTOR, 'canvas')
             initial_canvas_content = canvas_shadow.get_attribute('toDataURL')  # Armazene o conteúdo inicial do canvas
 
             # Simula o envio de uma mensagem com a imagem no navegador do usuário que envia
@@ -43,12 +44,17 @@ class ChatTests(unittest.TestCase):
             clica enter
             -> y entrou no chat  """
             
-            textarea_send = WebDriverWait(driver_send, timeout=10).until(lambda d: d.find_element(By.TAG_NAME, 'textarea'))
+            textarea_send = shadow_root.find_element(By.CSS_SELECTOR, 'textarea')
             textarea_send.send_keys('Mensagem de teste')  # Digite a mensagem na textarea
             textarea_send.send_keys(u'\ue007')
-            input_send = WebDriverWait(driver_send, timeout=10).until(lambda d: d.find_element(By.TAG_NAME, 'input'))
+            driver_send.implicitly_wait(30)
+            input_send = shadow_root.find_element(By.CSS_SELECTOR, 'input')
+            driver_send.implicitly_wait(30)
             input_send.send_keys('UsuarioTeste')  # Digite a mensagem na textarea
-            textarea_send.send_keys(u'\ue007')
+            input_send.send_keys(u'\ue007')
+            actions = ActionChains(driver_send)
+            actions.send_keys(u'\ue007')
+            actions.perform()
 
             # Registra o tempo de envio da mensagem
             send_time = time.time()
